5853bc65a84c15b8770b32915f86fc24ab496aec
Rodrigo Bossini <professorbossini@gmail.com>
2020-08-27 16:28:34 -0300

    Estamos na apostila 02-02. REtomar do item 2.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'fatec-ipi-paoo-diretivas';
+  textoBotao: string = 'Esconder';
+  esconder: boolean = false;
+
+  pessoas = [
+    { nome: 'José', idade: 18 },
+    { nome: 'Maria', idade: 22 },
+    { nome: 'Ana', idade: 30 },
+  ];
+
+  adicionar(n: string, i: number) {
+    this.pessoas.push({ nome: n, idade: i });
+  }
+
+  alterarExibicao(): void {
+    this.textoBotao = this.esconder ? 'Exibir' : 'Esconder';
+    this.esconder = !this.esconder;
+  }
 }
